a3876c31a0cf30cb6029b7bbc818d1799e1ba839
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2022-12-27 16:26:43 +1100

    index.js for user to receive the question is done

--- main/index.js
+++ main/index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkup')
 
-// TODO: Create an array of questions for user input
+//Create an array of questions for user input
 const questions =[
     {
         type: 'input',
@@ -13,51 +13,68 @@ const questions =[
     {
         type: 'input',
         name: 'description',
-        message: 'What is your email adress?',
+        message: 'Write a brief description about the project?',
     },
     {
         type: 'input',
-        name: 'project',
+        name: 'installation',
         message: 'Write a brief description about the project?',
     },
     {
         type: 'input',
         name: 'description',
-        message: 'please write a short description of your project',
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'What kind of license should your project have?',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+        message: 'How is your project installed?',
+        default: "N/A",
     },
     {
         type: 'input',
-        name: 'dependencies',
-        message: 'What command should be run to install dependencies?',
-        default: ['npm i']
+        name: 'usage',
+        message: 'How will your project will be used?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project use?',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can people contribute to your project?',
+        default: 'For contribution, use Github to fork the repository to submit suggested changes or changes using feature branches'
     },
     {
         type: 'input',
         name: 'test',
         message: 'What command should be run to test?',
-        default: ['npm test']
+        default: 'npm run test'
     },
     {
         type: 'input',
-        name: 'repo',
-        message: 'What does the user need to know about using the repo?',
+        name: 'github',
+        message: 'what is your Github username?',
     },
     {
         type: 'input',
-        name: 'contributing',
-        message: 'What does the user need to know about contributing to the repo?',
+        name: 'email',
+        message: 'what is your email address?',
     },
-]
-.then((answers) => {
-    const readmeContent = generateREADME(answers); 
+];
 
-    fs.writeFile('README.md', readmeContent, (err) =>
-    err ? console.log(err) : console.log('Generating README...!')
-    ); 
-});
+//Create a function to initialize app
+function init() {
+   // Use inquirer for question to start
+    inquirer
+    .createPromptModule(questions)
+    // Gather the answers and send to write the file
+    .then((answers) => {
+        const generateMarkdown = generateREADME(answers); 
+    
+        fs.writeFile('README.md', generateMarkdown, (err) =>
+        err ? console.log(err) : console.log('Generating README...!')
+        ); 
+    });
+};
+
+// Function call to start the app
+init();
